0475c895ee70b920d7038ad805cb804913f640c9
neves350 <martimneves350@gmail.com>
2026-03-02 16:51:50 +0000

    refactor(auth): refactor login page ui/ux

--- web/src/app/pages/login/login.ts
+++ web/src/app/pages/login/login.ts
@@ -3,33 +3,42 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { Router, RouterLink } from '@angular/router'
 import { AuthService } from '@core/services/auth/auth.service'
 import {
+	ArrowRightIcon,
 	LockKeyholeIcon,
 	LucideAngularModule,
 	MailIcon,
 	WalletMinimalIcon,
 } from 'lucide-angular'
-import { NgxSonnerToaster, toast } from 'ngx-sonner'
+import { toast } from 'ngx-sonner'
+import { ZardAvatarComponent } from '@/shared/components/ui/avatar'
+import { ZardButtonComponent } from '@/shared/components/ui/button'
+import { ZardDividerComponent } from '@/shared/components/ui/divider'
+import { ZardInputDirective } from '@/shared/components/ui/input'
 
 @Component({
 	selector: 'app-login',
-	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [
 		LucideAngularModule,
 		ReactiveFormsModule,
-		NgxSonnerToaster,
 		RouterLink,
+		ZardAvatarComponent,
+		ZardButtonComponent,
+		ZardDividerComponent,
+		ZardInputDirective,
 	],
 	templateUrl: './login.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Login {
-	readonly WalletMinimalIcon = WalletMinimalIcon
-	readonly MailIcon = MailIcon
-	readonly LockKeyholeIcon = LockKeyholeIcon
-	readonly toast = toast
-
 	private readonly fb = inject(FormBuilder)
 	private readonly router = inject(Router)
 	private readonly authService = inject(AuthService)
+
+	readonly WalletMinimalIcon = WalletMinimalIcon
+	readonly MailIcon = MailIcon
+	readonly LockKeyholeIcon = LockKeyholeIcon
+	readonly ArrowRightIcon = ArrowRightIcon
+	readonly toast = toast
 
 	form = this.fb.nonNullable.group({
 		email: ['', [Validators.email, Validators.required]],
