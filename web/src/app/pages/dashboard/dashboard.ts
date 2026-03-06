a4b4b99f5e6bfa84258b306dc8f400a6e207c6a5
neves350 <martimneves350@gmail.com>
2026-03-06 21:34:46 +0000

    feat(dashboard): add period event + cashflow component

--- web/src/app/pages/dashboard/dashboard.ts
+++ web/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { DashboardCard } from '@/shared/components/dashboard/dashboard-card/dashboard-card'
 import { DashboardCards } from '@/shared/components/dashboard/dashboard-cards/dashboard-cards'
+import { DashboardCashflow } from '@/shared/components/dashboard/dashboard-cashflow/dashboard-cashflow'
 import { DashboardChart } from '@/shared/components/dashboard/dashboard-chart/dashboard-chart'
 import { DashboardHeader } from '@/shared/components/dashboard/dashboard-header/dashboard-header'
 import { DashboardTransactions } from '@/shared/components/dashboard/dashboard-transactions/dashboard-transactions'
+import {
+	PeriodType,
+	type StatisticsQueryParams,
+} from '@core/api/statistics.interface'
+import { StatisticsService } from '@core/services/statistics.service'
 
 @Component({
 	selector: 'app-dashboard',
@@ -13,7 +19,18 @@ import { DashboardTransactions } from '@/shared/components/dashboard/dashboard-t
 		DashboardCards,
 		DashboardTransactions,
 		DashboardHeader,
+		DashboardCashflow,
 	],
 	templateUrl: './dashboard.html',
 })
-export class Dashboard {}
+export class Dashboard {
+	private readonly statisticsService = inject(StatisticsService)
+
+	constructor() {
+		this.statisticsService.loadStatistics({ period: PeriodType.MONTH })
+	}
+
+	onFilterChange(params: StatisticsQueryParams): void {
+		this.statisticsService.loadStatistics(params)
+	}
+}
