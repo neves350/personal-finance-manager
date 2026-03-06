80dcf6cdcfe2c48e14db6b1a359c51a9c3513f5a
neves350 <martimneves350@gmail.com>
2026-03-06 20:54:54 +0000

    feat(dashboard): add period selector to header

--- web/src/app/shared/components/dashboard/dashboard-header/dashboard-header.ts
+++ web/src/app/shared/components/dashboard/dashboard-header/dashboard-header.ts
@@ -1,7 +1,18 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
+import {
+	ChangeDetectionStrategy,
+	Component,
+	inject,
+	output,
+	signal,
+} from '@angular/core'
+import {
+	PeriodType,
+	type StatisticsQueryParams,
+} from '@core/api/statistics.interface'
 import { AuthService } from '@core/services/auth/auth.service'
 import {
 	ArrowRightLeftIcon,
+	CalendarDaysIcon,
 	LucideAngularModule,
 	PlusIcon,
 } from 'lucide-angular'
@@ -9,10 +20,16 @@ import { BankAccountsForm } from '../../bank-accounts/bank-accounts-form/bank-ac
 import { TransfersForm } from '../../transfers/transfers-form/transfers-form'
 import { ZardButtonComponent } from '../../ui/button'
 import { ZardDialogService } from '../../ui/dialog'
+import { ZardSelectComponent, ZardSelectItemComponent } from '../../ui/select'
 
 @Component({
 	selector: 'app-dashboard-header',
-	imports: [ZardButtonComponent, LucideAngularModule],
+	imports: [
+		ZardButtonComponent,
+		LucideAngularModule,
+		ZardSelectComponent,
+		ZardSelectItemComponent,
+	],
 	templateUrl: './dashboard-header.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -20,9 +37,24 @@ export class DashboardHeader {
 	private readonly dialogService = inject(ZardDialogService)
 	private readonly authService = inject(AuthService)
 
+	readonly user = this.authService.currentUser
 	readonly ArrowRightLeftIcon = ArrowRightLeftIcon
 	readonly PlusIcon = PlusIcon
-	readonly user = this.authService.currentUser
+	readonly CalendarDaysIcon = CalendarDaysIcon
+
+	readonly period = signal<PeriodType>(PeriodType.MONTH)
+	readonly filterChange = output<StatisticsQueryParams>()
+
+	onPeriodChange(value: string | string[]) {
+		this.period.set(value as PeriodType)
+		this.emitFilter()
+	}
+
+	private emitFilter() {
+		this.filterChange.emit({
+			period: this.period(),
+		})
+	}
 
 	openTransfer() {
 		this.dialogService.create({
