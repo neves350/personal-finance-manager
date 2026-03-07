5f2e9f7eede28b2d41b2c95748dd8a066b177272
neves350 <martimneves350@gmail.com>
2026-03-07 01:44:02 +0000

    feat(dashboard): add dashboard goals component

--- web/src/app/pages/dashboard/dashboard.ts
+++ web/src/app/pages/dashboard/dashboard.ts
@@ -5,12 +5,18 @@ import {
 } from '@core/api/statistics.interface'
 import { StatisticsService } from '@core/services/statistics.service'
 import { DashboardCashflow } from '@/shared/components/dashboard/dashboard-cashflow/dashboard-cashflow'
+import { DashboardGoals } from '@/shared/components/dashboard/dashboard-goals/dashboard-goals'
 import { DashboardHeader } from '@/shared/components/dashboard/dashboard-header/dashboard-header'
 import { DashboardSpending } from '@/shared/components/dashboard/dashboard-spending/dashboard-spending'
 
 @Component({
 	selector: 'app-dashboard',
-	imports: [DashboardHeader, DashboardCashflow, DashboardSpending],
+	imports: [
+		DashboardHeader,
+		DashboardCashflow,
+		DashboardSpending,
+		DashboardGoals,
+	],
 	templateUrl: './dashboard.html',
 })
 export class Dashboard {
