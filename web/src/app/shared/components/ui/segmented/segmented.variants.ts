33cd383e94b9eef7aeaab30016201a965704c658
neves350 <martimneves350@gmail.com>
2026-03-26 12:17:22 +0000

    refactor(ui): add cursor-pointer to segmented variant

--- web/src/app/shared/components/ui/segmented/segmented.variants.ts
+++ web/src/app/shared/components/ui/segmented/segmented.variants.ts
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
 export const segmentedVariants = cva(
-	'inline-flex items-center justify-center rounded-md bg-card p-1 text-muted-foreground',
+	'inline-flex items-center justify-center rounded-md border bg-card p-1 text-muted-foreground',
 	{
 		variants: {
 			zSize: {
@@ -17,7 +17,7 @@ export const segmentedVariants = cva(
 )
 
 export const segmentedItemVariants = cva(
-	'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+	'cursor-pointer inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	{
 		variants: {
 			zSize: {
