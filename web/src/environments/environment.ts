cca3c4627caf7b72a972a3d4ca3fe27e490d676c
neves350 <martimneves350@gmail.com>
2026-06-26 00:04:45 +0100

    chore: add render api url

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	apiUrl: 'http://localhost:3000',
+	apiUrl: 'https://personal-finance-manager-ff9g.onrender.com',
 	googleClientId:
 		'495086227507-1uc6ugumv1qstu2v2ekgls09gpjda0t8.apps.googleusercontent.com',
 }
