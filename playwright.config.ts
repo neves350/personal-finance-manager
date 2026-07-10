858b85ca2e09300ad104e3ef39c57e77042266e5
neves350 <martimneves350@gmail.com>
2026-07-10 01:08:03 +0100

    chore: configure Playwright for e2e tests

---
+++ playwright.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig, devices } from "@playwright/test"
+
+export default defineConfig({
+	testDir: "./e2e/tests",
+
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+
+	reporter: "html",
+
+	globalSetup: "./e2e/setup/global-setup.ts",
+	globalTeardown: "./e2e/setup/global-teardown.ts",
+
+	use: {
+		baseURL: "http://localhost:4200",
+		trace: "on-first-retry",
+	},
+
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
+
+	webServer: [
+		{
+			command: "npm run dev:api",
+			url: "http://localhost:3000",
+			reuseExistingServer: !process.env.CI,
+		},
+		{
+			command: "npm run dev:web",
+			url: "http://localhost:4200",
+			reuseExistingServer: !process.env.CI,
+		},
+	],
+})
