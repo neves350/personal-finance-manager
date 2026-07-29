26200175c3544ae51a54cca24d92fce0ae8aad37
neves350 <martimneves350@gmail.com>
2026-07-30 00:30:12 +0100

    feat(web): add footer landing-page

--- apps/web/src/app/pages/landing-page/landing-page.ts
+++ apps/web/src/app/pages/landing-page/landing-page.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { Features } from '@/shared/components/landing/features/features'
+import { Footer } from '@/shared/components/landing/footer/footer'
 import { Gallery } from '@/shared/components/landing/gallery/gallery'
 import { Hero } from '@/shared/components/landing/hero/hero'
 import { HowWorks } from '@/shared/components/landing/how-works/how-works'
@@ -7,7 +8,7 @@ import { Navbar } from '@/shared/components/landing/navbar/navbar'
 
 @Component({
 	selector: 'app-landing-page',
-	imports: [Navbar, Hero, Features, HowWorks, Gallery],
+	imports: [Navbar, Hero, Features, HowWorks, Gallery, Footer],
 	templateUrl: './landing-page.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
