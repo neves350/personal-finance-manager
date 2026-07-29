598086bfd48c28f247e13dd0ce85174065a56411
neves350 <martimneves350@gmail.com>
2026-07-30 00:10:10 +0100

    feat(web): add gallery landing-page

--- apps/web/src/app/pages/landing-page/landing-page.ts
+++ apps/web/src/app/pages/landing-page/landing-page.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { Features } from '@/shared/components/landing/features/features'
+import { Gallery } from '@/shared/components/landing/gallery/gallery'
 import { Hero } from '@/shared/components/landing/hero/hero'
 import { HowWorks } from '@/shared/components/landing/how-works/how-works'
 import { Navbar } from '@/shared/components/landing/navbar/navbar'
 
 @Component({
 	selector: 'app-landing-page',
-	imports: [Navbar, Hero, Features, HowWorks],
+	imports: [Navbar, Hero, Features, HowWorks, Gallery],
 	templateUrl: './landing-page.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
