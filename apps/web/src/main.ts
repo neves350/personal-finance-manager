5e3761d8e4712c8360aa9cb393cbe0aef133ef83
neves350 <martimneves350@gmail.com>
2026-07-31 00:05:42 +0100

    chore(web): add @vercel/speed-insights

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,5 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import { App } from './app/app'
 import { appConfig } from './app/app.config'
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err))
+injectSpeedInsights()
